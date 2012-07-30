6b775e8c5b092c0132a37e7dd76372a9ba892634
bikashbhusal98 <bikashbhusal98@gmail.com>
2012-07-29 21:52:05 -0400

    Menu with layout and class

---
+++ Inventory Management/src/com/example/inventory/management/Menu.java
@@ -0,0 +1,13 @@
+package com.example.inventory.management;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);
+	}
+
+}
